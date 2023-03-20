14ffc1ae29b90d6577212ca796c4723b37ae5d78
zertmark <47692978+zertmark@users.noreply.github.com>
2023-03-20 20:39:17 +0300

    Add files via upload

--- fast_detection.py
+++ fast_detection.py
@@ -32,8 +32,7 @@ class NetModel(cv2.dnn_DetectionModel):
             if len(classIds):
                 for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
                     cv2.rectangle(image,box,color=(0,255,0),thickness=2)
-                    if classId and classId <= self.classNamesLen:
-                        cv2.putText(image,self.classNames[classId-1].upper(),(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(image,self.classNames[classId-1].upper(),(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
 
     def detectObjectsInWebCam(self, frameName:str, Webcam:WebCam) -> None:
         while True:
