f96e314939a3379f3f0d6abd78f9fb898ed53680
zertmark <47692978+zertmark@users.noreply.github.com>
2023-03-21 01:28:46 +0300

    Add files via upload

--- fast_detection.py
+++ fast_detection.py
@@ -1,6 +1,6 @@
 import cv2
 import os
-
+import numpy as np
 class WebCam(cv2.VideoCapture):
     def __init__(self, frameName:str = "test_frame"):
         super().__init__(1)
@@ -11,28 +11,36 @@ class WebCam(cv2.VideoCapture):
         self.frameName = frameName
 
 class NetModel(cv2.dnn_DetectionModel):
-    def __init__(self, weightsPath:str, configPath:str, classNamesPath:str, thres:float) -> None:
+    def __init__(self, weightsPath:str, configPath:str, classNamesPath:str, thres:float = 0.45, nmsUse:bool = False) -> None:
         super().__init__(weightsPath, configPath)
         self.setInputSize(320,320)
         self.setInputScale(1.0/ 127.5)
         self.setInputMean((127.5, 127.5, 127.5))
         self.setInputSwapRB(True)
         self.thres:float = 0.45
+        self.nms_threshold = 0.2
         self.weightsPath:str = weightsPath    
         self.configPath:str = configPath    
         self.classNamesPath:str = classNamesPath
         self.classNames:list = self.readClassNames()    
         self.classNamesLen:int = len(self.classNames)
+        self.nmsUse = nmsUse
 
     def readClassNames(self) -> None:
         return open(self.classNamesPath, "rt", encoding="UTF-8").read().rstrip("\n").split("\n")
 
+
+    def getBoxesIDsNMS(self, bbox, confs) -> list:
+        return cv2.dnn.NMSBoxes(list(bbox), list(map(float, list(np.array(confs).reshape(1,-1)[0]))), self.thres, self.nms_threshold)
+
     def detectObjectsInImage(self, image) -> object:
             classIds, confs, bbox = self.detect(image,confThreshold=self.thres)
+            #Shit code
+            bboxIDs = self.getBoxesIDsNMS(bbox, confs) if self.nmsUse else range(len(bbox))
             if len(classIds):
-                for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-                    cv2.rectangle(image,box,color=(0,255,0),thickness=2)
-                    cv2.putText(image,self.classNames[classId-1].upper(),(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                for classId, confidence,boxId in zip(classIds.flatten(),confs.flatten(),bboxIDs):
+                    cv2.rectangle(image,bbox[boxId],color=(0,255,0),thickness=2)
+                    cv2.putText(image,self.classNames[classId-1].upper(),(bbox[boxId][0]+10,bbox[boxId][1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
 
     def detectObjectsInWebCam(self, frameName:str, Webcam:WebCam) -> None:
         while True:
@@ -46,7 +54,7 @@ if __name__ == "__main__":
     configPath = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
     weightsPath = "frozen_inference_graph.pb"
     classIdsPath = "coco.names"
-    net = NetModel(weightsPath, configPath, classIdsPath, thres=0.45)
+    net = NetModel(weightsPath, configPath, classIdsPath, thres=0.45, nmsUse=True)
     cam = WebCam()
     net.readClassNames()
     net.detectObjectsInWebCam(cam.frameName, cam)
